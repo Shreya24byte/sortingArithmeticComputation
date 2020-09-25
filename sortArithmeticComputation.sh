26e0a70689399940e692d0c064dadab3c6b969b3
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-25 14:09:36 +0530

    Add: take user input

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation Program";
+
+#User input
+read -p "Enter the first number:" num1;
+read -p "Enter the second number:" num2;
+read -p "Enter the third number:" num3;
