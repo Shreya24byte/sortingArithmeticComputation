983707b60d95d1f8a0689980cd2906678f67c390
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-25 14:22:22 +0530

    Add: second computation a*b+c

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -6,6 +6,9 @@ read -p "Enter the first number:" num1;
 read -p "Enter the second number:" num2;
 read -p "Enter the third number:" num3;
 
-#Computation
+#Computations
 computation1=$(($num1 + $num2 * $num3));
 echo "a+b*c =" $computation1;
+
+computation2=$(($num1 * $num2 + $num3));
+echo "a*b+c =" $computation2;
