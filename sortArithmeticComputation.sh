125cb6d4515f799f7581f67cde550ebd43fbc213
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-25 14:18:21 +0530

    Add: first computation a+b*c

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -5,3 +5,7 @@ echo "Welcome to Sorting Arithmetic Computation Program";
 read -p "Enter the first number:" num1;
 read -p "Enter the second number:" num2;
 read -p "Enter the third number:" num3;
+
+#Computation
+computation1=$(($num1 + $num2 * $num3));
+echo "a+b*c =" $computation1;
